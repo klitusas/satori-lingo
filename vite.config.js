155b1934cb558e399ab58f22cb70081229a27677
klitusas <klitusas@gmail.com>
2026-06-18 11:07:40 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [
         react({
             jsxRuntime: 'automatic'
         })
     ],
-    base: process.env.GITHUB_PAGES ? '/satori-lingo/' : '/',
+    base: command === 'build' ? '/satori-lingo/' : '/',
     server: {
         port: 3000,
         open: true
@@ -15,4 +15,4 @@ export default defineConfig({
     build: {
         outDir: 'dist'
     }
-});
+}));
